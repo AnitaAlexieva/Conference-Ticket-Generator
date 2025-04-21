0252213abe66d9f9a048d6ea46f42c8c7c6f7cb6
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-21 23:24:27 +0300

    Fix the attribution component

--- client/src/components/Attribution.jsx
+++ client/src/components/Attribution.jsx
@@ -1,6 +1,6 @@
 export default function Attribution() {
     return(
-        <div class="attribution">
+        <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
       </div>
