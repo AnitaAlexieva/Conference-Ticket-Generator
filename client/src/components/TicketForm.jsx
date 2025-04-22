362069fb3c65fd5262b7e8a5778d851c6e285687
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-22 16:54:48 +0300

    Style all headings, lables and paragraphs

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -1,37 +1,38 @@
 export default function TicketForm() {
-    return(
-       <div className="app-wrapper">
+    return (
+        <div className="app-wrapper">
             <div className="content-container">
                 <div className="header">
                     <div className="mini-text">
-                        <img src="/public/images/logo-full.svg" alt="logo"/>
+                        <img src="/public/images/logo-full.svg" alt="logo" />
                     </div>
 
                     <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
-                    <p className="header-p">Secure your spot at next year's biggest coding conference</p>
+                    <h4 className="header-p">Secure your spot at next year's biggest coding conference.</h4>
                 </div>
-            <div className="ticket-container">
-                <form>
-                    <label htmlFor="avatar">Upload Avatar</label>
-                    <div>
-                        <input type="text" name="avatar"/>
-                        <h4>Drag and drop or click to upload</h4>
-                    </div>
-                    <p>ICON  Upload your photo (JPG or PNG, max size: 500KB).</p>
-                    
-                    <label htmlFor="name">Full Name</label>
-                    <input type="text" name="name" />
+                <div className="ticket-container">
+                    <form>
+                        <label htmlFor="avatar">Upload Avatar</label>
+                        <div>
+                            <button name="avatar">Add</button>
+                            <h5>Drag and drop or click to upload</h5>
+                        </div>
+                            <p className="info-p"><img src="/public/images/icon-info.svg" alt="info" />  Upload your photo (JPG or PNG, max size: 500KB).</p>
 
-                    <label htmlFor="">Email Address</label>
-                    <input type="email" name="email" />
+                        <label htmlFor="name">Full Name</label>
+                        <input type="text" name="name" />
 
-                    <label htmlFor="gitHub">GitHub Username</label>
-                    <input type="text" name="gitHub" />
+                        <label htmlFor="">Email Address</label>
+                        <input type="email" name="email" />
 
-                    <button type="submit">Generate My Ticket</button>
-                </form>
+                        <label htmlFor="gitHub">GitHub Username</label>
+                        <input type="text" name="gitHub" />
+
+                        <button type="submit">Generate My Ticket</button>
+                    </form>
+                </div>
+
             </div>
-
             <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
                 Coded by <a href="#">Your Name Here</a>.
