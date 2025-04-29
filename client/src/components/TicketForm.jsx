7e5906fff8cf46038c069f72b58fa3627f085d10
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-29 22:18:20 +0300

    Change the upload button with an input with type file

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -5,20 +5,25 @@ export default function TicketForm() {
 
     const getInfo = (e) => {
         e.preventDefault();
-    
+
         const formData = new FormData(e.target);
         const name = formData.get('name');
         const email = formData.get('email');
         const github = formData.get('gitHub');
-      
+
         if (!name || !email || !github) {
             setError("All fields are required!");
             return;
         }
-      
+
         navigate("/ticket", { state: { info: { name, email, github } } });
     };
-    
+
+    const handleFileUpload = (e) =>{
+        const file = e.target.files;
+        console.log(file)
+    }
+
     return (
         <div className="app-wrapper">
             <div className="content-container">
@@ -34,19 +39,23 @@ export default function TicketForm() {
                     <form onSubmit={getInfo}>
                         <label htmlFor="avatar">Upload Avatar</label>
                         <div className="avatar-div" tabIndex="0">
-                            <button className="avatar-btn"><img src="/images/icon-upload.svg" alt="upload" /></button>
+                            <label htmlFor="avatar-upload" className="avatar-btn">
+                                <img src="/images/icon-upload.svg" alt="upload" />
+                            </label>
+                            <input id="avatar-upload" type="file" onChange={handleFileUpload} style={{ display: 'none' }} />
                             <h5>Drag and drop or click to upload</h5>
                         </div>
-                            <p className="info-p"><img src="/images/icon-info.svg" alt="info" />  Upload your photo (JPG or PNG, max size: 500KB).</p>
+
+                        <p className="info-p"><img src="/images/icon-info.svg" alt="info" />  Upload your photo (JPG or PNG, max size: 500KB).</p>
 
                         <label htmlFor="name">Full Name</label>
                         <input type="text" name="name" />
 
                         <label htmlFor="">Email Address</label>
-                        <input type="email" name="email" placeholder="example@gmail.com"/>
+                        <input type="email" name="email" placeholder="example@gmail.com" />
 
                         <label htmlFor="gitHub">GitHub Username</label>
-                        <input type="text" name="gitHub" placeholder="@yourusername"/>
+                        <input type="text" name="gitHub" placeholder="@yourusername" />
 
                         <button className="submit" type="submit" >Generate My Ticket</button>
                     </form>
